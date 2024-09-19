8c0333d62462f6395470c4f1c9af00f1648e65e6
Markus Ressel <mail@markusressel.de>
2024-09-20 00:08:11 +0200

    disable interactive mode in ci

--- run-container.sh
+++ run-container.sh
@@ -10,4 +10,9 @@ mkdir -p "$REPOSITORY_PATH"
 
 sudo docker buildx build --tag archbuild --build-arg LTS="$VARIANT" .
 mkdir -p "$REPOSITORY_PATH"
-sudo docker run -i -t --rm -v "$REPOSITORY_PATH:/packages" archbuild
+
+if [[ -v GITHUB_ACTIONS ]]; then
+  sudo docker run --rm -v "$REPOSITORY_PATH:/packages" archbuild
+else
+  sudo docker run -i -t --rm -v "$REPOSITORY_PATH:/packages" archbuild
+fi
